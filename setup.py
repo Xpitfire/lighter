93297300cce4979e09d1f8b2fc91bbbe919fbe11
Xpitfire <dinu.marius-constantin@hotmail.com>
2019-11-06 16:18:36 +0100

    Initial project files commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+requirements = {"install": ["ffmpeg", "natsort", "numpy", "tqdm", "imageio", "opencv-python",
+                            "torch", "torchvision", "facenet-pytorch", "tensorboard", "coloredlogs",
+                            "petname"]}
+
+install_requires = requirements["install"]
+
+setup(
+    # Metadata
+    name="lighter",
+    author="Marius-Constantin Dinu",
+    version="0.0.1",
+    author_email="dinu.marius-constantin@hotmail.com",
+    url="https://github.com/Xpitfire/lighter",
+    description="Lightweight reinforcement learning framework for PyTorch",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    # Package info
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent"
+    ]
+)
