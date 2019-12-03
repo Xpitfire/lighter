d98a8eccfb8dcdb6b4cc6972447c2f3846f4ea35
Xpitfire <dinu.marius-constantin@hotmail.com>
2019-12-03 15:59:44 +0100

    changed implementation

--- examples/coco_looking/models/alexnet.py
+++ examples/coco_looking/models/alexnet.py
@@ -8,18 +8,12 @@ class AlexNetFeatureExtractionModel(BaseModule):
     @config(path='models/alexnet.config.json', property='model')
     def __init__(self):
         super(AlexNetFeatureExtractionModel, self).__init__()
-        self.alexnet = models.alexnet(pretrained=self.config.model.pretrained).to(self.device)
+        self.alexnet = models.alexnet(pretrained=self.config.model.pretrained)
         for param in self.alexnet.parameters():
             param.requires_grad = not self.config.model.freeze_pretrained
-        self.hidden = torch.nn.Linear(self.alexnet.classifier[-1].out_features,
-                                      self.config.model.hidden_units).to(self.device)
-        self.final = torch.nn.Linear(self.config.model.hidden_units,
-                                     self.config.model.output).to(self.device)
+        self.alexnet.classifier._modules['6'] = torch.nn.Linear(4096, self.config.model.output)
+        self.alexnet = self.alexnet.to(self.device)
 
     def forward(self, x):
         h = self.alexnet(x)
-        h = torch.relu(h)
-        h = self.hidden(h)
-        h = torch.relu(h)
-        h = self.final(h)
         return torch.sigmoid(h)
