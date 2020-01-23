b186465901021d278c4ba530a237eaebd95a116d
Xpitfire <dinu.marius-constantin@hotmail.com>
2020-01-23 12:57:36 +0100

    Added new features for the xda activation function

--- lighter/nn/xda.py
+++ lighter/nn/xda.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 class XdA(torch.nn.Module):
@@ -22,7 +23,9 @@ class XdA(torch.nn.Module):
     def __init__(self, alpha: float = 0.01,
                  beta: float = 0.0,
                  mode: str = 'mean',
-                 norm_type: str = 'layernorm'):
+                 norm_type: str = 'layernorm',
+                 mask_type: str = 'mask',
+                 mse_lr: float = 1e-2):
         """
         Context-dependent activations implementation using normalized activation space summary statistics
         for the non-linearity definition.
@@ -34,6 +37,8 @@ class XdA(torch.nn.Module):
         :param beta: The beta parameter regulates the leakyness of the gating non-linearity.
         :param mode: The summary statistic
         :param norm_type: Normalize the x values according to the given normalization type before activation
+        :param mse_lr: Learning rate for the differential parameters in the 'mse' mode.
+        :param mask_type: Select the gating mode.
         """
         super(XdA, self).__init__()
         self.alpha = alpha
@@ -42,18 +47,33 @@ class XdA(torch.nn.Module):
         self.phi = None
         self.norm = None
         self.norm_type = norm_type
+        self.mask_type = mask_type
+        self.mse_lr = mse_lr
         assert 0 <= self.alpha <= 1
         assert 0 <= self.beta <= 1
-        assert self.mode in ['mean', 'median']
-        assert self.norm_type in [None, 'vecnorm', 'layernorm', 'mean', 'std']
+        assert 0 <= self.mse_lr
+        assert self.mode in ['zero', 'mean', 'median', 'mse']
+        assert self.norm_type in [None, 'vecnorm', 'batchnorm', 'layernorm', 'multinorm', 'mean', 'std']
+        assert self.mask_type in ['mask', 'max']
 
-    def _mask(self, x):
+    @staticmethod
+    def _multi_norm(x, eps=1e-7):
         """
-        Creates a binary mask according to the cumulative summary statistics.
-        :param x: teh activation space inputs
-        :return: binary mask with same dimensions as the inputs
+        Computes the batch and layer norm for the committed value x.
         """
-        return (x >= self.phi.expand_as(x)).float()
+        b_mean = torch.mean(x, dim=0, keepdim=True)
+        b_std = torch.sqrt(torch.var(x, dim=0, keepdim=True) + eps)
+        x = (x - b_mean) / b_std
+        x_shape = x.shape
+        if len(x_shape) == 2:
+            x = (x - torch.mean(x, dim=-1, keepdim=True)) / torch.sqrt(torch.var(x, dim=-1, keepdim=True) + eps)
+        elif len(x_shape) > 2:
+            x = x.view(x_shape[0], -1)
+            x = (x - torch.mean(x, dim=-1, keepdim=True)) / torch.sqrt(torch.var(x, dim=-1, keepdim=True) + eps)
+            x = x.view(x_shape)
+        else:
+            raise NotImplementedError('Unsupported normalization shape: {}'.format(x_shape))
+        return x
 
     @staticmethod
     def _layer_norm(x, eps=1e-7):
@@ -72,6 +92,16 @@ class XdA(torch.nn.Module):
         return x
 
     @staticmethod
+    def _batch_norm(x, eps=1e-7):
+        """
+        Computes the batch norm for the committed value x.
+        """
+        b_mean = torch.mean(x, dim=0, keepdim=True)
+        b_std = torch.sqrt(torch.var(x, dim=0, keepdim=True) + eps)
+        x = (x - b_mean) / b_std
+        return x
+
+    @staticmethod
     def _vec_norm(x, p=2, eps=1e-7):
         """
         Computes the normalized vector for the committed value x.
@@ -85,6 +115,8 @@ class XdA(torch.nn.Module):
             x_norm = torch.pow(torch.sum(torch.pow(x, p), dim=-1, keepdim=True) + eps, 1/p)
             x = x / x_norm
             x = x.view(x_shape)
+        else:
+            raise NotImplementedError('Unsupported normalization shape: {}'.format(x_shape))
         return x
 
     @staticmethod
@@ -99,6 +131,8 @@ class XdA(torch.nn.Module):
             x = x.view(x_shape[0], -1)
             x = x - torch.mean(x, dim=1, keepdim=True)
             x = x.view(x_shape)
+        else:
+            raise NotImplementedError('Unsupported normalization shape: {}'.format(x_shape))
         return x
 
     @staticmethod
@@ -115,6 +149,8 @@ class XdA(torch.nn.Module):
             x_std = torch.sqrt(torch.var(x, dim=1, keepdim=True) + eps)
             x = x / x_std
             x = x.view(x_shape)
+        else:
+            raise NotImplementedError('Unsupported normalization shape: {}'.format(x_shape))
         return x
 
     def _check_init(self, x):
@@ -128,21 +164,30 @@ class XdA(torch.nn.Module):
             # init normalization
             if self.norm_type == 'vecnorm':
                 self.norm = XdA._vec_norm
+            elif self.norm_type == 'batchnorm':
+                self.norm = XdA._layer_norm
             elif self.norm_type == 'layernorm':
                 self.norm = XdA._layer_norm
+            elif self.norm_type == 'multinorm':
+                self.norm = XdA._multi_norm
             elif self.norm_type == 'mean':
                 self.norm = XdA._mean
             elif self.norm_type == 'std':
                 self.norm = XdA._std
             # init phi parameters
-            self.phi = torch.nn.Parameter(torch.zeros(x.shape[1:]), requires_grad=False).to(x.device)
-            if self.mode == 'mean':
+            if self.mode == 'zero':
+                self.phi = torch.nn.Parameter(torch.zeros(x.shape[1:]), requires_grad=False).to(x.device)
+            elif self.mode == 'mean':
+                self.phi = torch.nn.Parameter(torch.zeros(x.shape[1:]), requires_grad=False).to(x.device)
                 x_ = x.detach().mean(0).data
+                self.phi.data = self.phi + x_
             elif self.mode == 'median':
+                self.phi = torch.nn.Parameter(torch.zeros(x.shape[1:]), requires_grad=False).to(x.device)
                 x_ = torch.median(x.detach(), dim=0)[0]
-            else:
-                raise NotImplementedError()
-            self.phi.data = self.phi + x_
+                self.phi.data = self.phi + x_
+            elif self.mode == 'mse':
+                self.phi = torch.from_numpy(np.random.randn(*x.shape[1:]).astype(np.float32)).to(x.device)
+                self.phi.requires_grad = True
             self.init = False
 
     def _update_phi(self, x):
@@ -154,11 +199,38 @@ class XdA(torch.nn.Module):
         if self.training:
             if self.mode == 'mean':
                 x_ = x.detach().mean(0)
+                self.phi.data = ((1 - self.alpha) * self.phi + self.alpha * x_).data
             elif self.mode == 'median':
                 x_ = torch.median(x.detach(), dim=0)[0]
+                self.phi.data = ((1 - self.alpha) * self.phi + self.alpha * x_).data
+            elif self.mode == 'mse':
+                self.phi.data -= self.mse_lr * (self.phi - x.detach()).mean()
+
+    def _mask(self, x):
+        """
+        Creates a binary mask according to the cumulative summary statistics.
+        :param x: teh activation space inputs
+        :return: binary mask with same dimensions as the inputs
+        """
+        return (x >= self.phi.expand_as(x)).float()
+
+    def _gate(self, x):
+        if self.mask_type == 'mask':
+            # creates a binary mask to disable units
+            mask = self._mask(x)
+            # disables units
+            h = x * mask
+            # checks if beta is enabled and applies a leaky variant
+            if self.beta != 0:
+                h += (self.beta * x * (1.0 - mask))
+        elif self.mask_type == 'max':
+            if self.beta == 0:
+                h = torch.max(x, self.phi.expand_as(x))
             else:
-                raise NotImplementedError()
-            self.phi.data = ((1 - self.alpha) * self.phi + self.alpha * x_).data
+                h = torch.max(x, self.beta * self.phi.expand_as(x))
+        else:
+            raise NotImplementedError('Invalid option!')
+        return h
 
     def forward(self, x):
         # check if layer is initialized
@@ -168,11 +240,5 @@ class XdA(torch.nn.Module):
             x = self.norm(x)
         # updates the gating summary statistic
         self._update_phi(x)
-        # creates a binary mask to disable units
-        mask = self._mask(x)
-        # disables units
-        h = x * mask
-        # checks if beta is enabled and applies a leaky variant
-        if self.beta > 0:
-            h += (self.beta * x * (1.0 - mask))
+        h = self._gate(x)
         return h
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # Metadata
     name="torch-lighter",
     author="Marius-Constantin Dinu",
-    version="0.2.18",
+    version="0.2.19",
     author_email="dinu.marius-constantin@hotmail.com",
     url="https://github.com/Xpitfire/lighter",
     scripts=['bin/lighter-init'],
